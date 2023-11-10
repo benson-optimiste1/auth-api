34ff14d411692c75dd85b2df5840be9d7bfb2ab1
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-10 11:59:38 -0500

    setup auth-api

---
+++ functions/src/middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { secretKey } from "./creds.js";
+
+export async function isAuthenticated(req, res, next){
+    const { authorization } = req.headers;
+    // first check IF THEY HAVE A TOKEN:
+    if(!authorization){
+        res.status(401).send({ message: 'No authorization token found' });
+        return;
+    }
+
+    // then check if the token is VALID:
+   jwt.verify(authorization, secretKey)
+   .then(decoded => { // valid token:
+    req.locals = decoded; // attach our decoded token to the request
+     // if so, go on
+     next();
+   })
+  
+   .catch(err => { // not valid token :
+    res.status(401).send(err);
+   })
+}
